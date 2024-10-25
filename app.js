8e17f9d0b709edefaaf56a1e76dfe4966cc7546d
MDBBee <bobbyugbebor@gmail.com>
2024-10-25 15:45:22 +0300

    Added relevant security features

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
@@ -28,6 +33,18 @@ const errorhandlerMiddleware = require("./middlewares/error-handler");
 const { authenticateUser } = require("./middlewares/authentication");
 
 //Express parsers
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
